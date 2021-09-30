bdfd9d8a4a62edf93d7f2bf0d15c27e0a75ba42d
eversolis <solisever777@gmail.com>
2021-09-30 17:39:19 -0600

    AddNote case added to NoteUseCases.kt

--- app/src/main/java/com/compose/noteappclean/core/use_case/NoteUseCases.kt
+++ app/src/main/java/com/compose/noteappclean/core/use_case/NoteUseCases.kt
@@ -2,5 +2,6 @@ package com.compose.noteappclean.core.use_case
 
 data class NoteUseCases(
     val getNotes: GetNotes,
-    val deleteNote: DeleteNote
+    val deleteNote: DeleteNote,
+    val addNote: AddNote
 )
