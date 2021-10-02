88da670bb9bc28daee6663f08559204737416761
eversolis <solisever777@gmail.com>
2021-10-02 17:20:52 -0600

    Companion object added to this class, adding a listo of colors to chose

--- app/src/main/java/com/compose/noteappclean/core/model/Note.kt
+++ app/src/main/java/com/compose/noteappclean/core/model/Note.kt
@@ -2,16 +2,21 @@ package com.compose.noteappclean.core.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.compose.noteappclean.presentation.theme.*
 import java.lang.Exception
 
 @Entity
 data class Note(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val title : String,
     val content: String,
     val timestamp: Long,
     val color: Int
-)
+){
+    companion object{
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
 
 class InvalidNoteException(message: String) : Exception(message)
